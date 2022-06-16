90a58b906c93f5b428691cd6fb9d6e69464506a6
rose <romartinez@council.nyc.gov>
2022-06-16 12:47:16 -0400

    added the vroom package, to read in the datasets faster

--- code/00_load_dependencies.R
+++ code/00_load_dependencies.R
@@ -5,7 +5,7 @@
 #' 
 #' IF YOU DO NOT WANT TO INSTALL ANY OF THESE PACKAGES, DO NOT RUN THIS CODE.
 
-list.of.packages <- c("tidyverse", "janitor", "sf")
+list.of.packages <- c("tidyverse", "janitor", "sf", "vroom")
 
 # checks if packages has been previously installed
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
